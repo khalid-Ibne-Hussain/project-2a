f09131567c39f085258e2f9d28721b644ee3e1e3
khalid-Ibne-Hussain <khalidtuhinbd@gmail.com>
2023-11-25 03:12:24 +0600

    user model, user interface created

---
+++ src/app/modules/user.model.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from 'mongoose';
+import { Order, User } from './user/user.interface';
+
+const orderSchema = new Schema<Order>({
+  productName: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+});
+
+const userSchema = new Schema<User>({
+  userId: { type: Number, required: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  fullName: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: Boolean, required: true },
+  hobbies: { type: [String], required: true },
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  orders: { type: [orderSchema], required: true },
+});
+
+const UserModel = model<User>('User', userSchema);
+
+export default UserModel;
